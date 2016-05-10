ec23eb0469e31debd8dfa7e4b58d8bf744e4e2bf
Søren Lundtoft <soren.lundtoft@gmail.com>
2016-05-10 12:57:05 +0200

    Path algorithm added to server

--- server.js
+++ server.js
@@ -11,6 +11,46 @@ var io = socketio.listen(server.server);
 var db = mongojs('intelligentProductSuggestions');
 var products = db.collection("products");
 
+//*********** Data array start *****************//
+var position = [];
+position[1] = [];
+position[2] = [];
+position[3] = [];
+position[4] = [];
+position[5] = [];
+
+position[1][1] = "x";
+position[1][2] = "57162e4ca9ced45b6cf2ce00";
+position[1][3] = "x";
+position[1][4] = "57162e4ca9ced45b6cf2cdf1";
+position[1][5] = "x";
+position[1][6] = "57162e4ca9ced45b6cf2cdf2";
+position[2][1] = "57162e4ca9ced45b6cf2ce04";
+position[2][2] = "57162e4ca9ced45b6cf2ce01";
+position[2][3] = "57162e4ca9ced45b6cf2ce08";
+position[2][4] = "57162e4ca9ced45b6cf2cdf8";
+position[2][5] = "57162e4ca9ced45b6cf2cdf3";
+position[2][6] = "57162e4ca9ced45b6cf2cdf4";
+position[3][1] = "57162e4ca9ced45b6cf2ce09";
+position[3][2] = "57162e4ca9ced45b6cf2ce07";
+position[3][3] = "57162e4ca9ced45b6cf2ce03";
+position[3][4] = "57162e4ca9ced45b6cf2cdf5";
+position[3][5] = "57162e4ca9ced45b6cf2cdf6";
+position[3][6] = "57162e4ca9ced45b6cf2cdf7";
+position[4][1] = "57162e4ca9ced45b6cf2ce02";
+position[4][2] = "57162e4ca9ced45b6cf2ce05";
+position[4][3] = "57162e4ca9ced45b6cf2ce06";
+position[4][4] = "57162e4ca9ced45b6cf2cdf9";
+position[4][5] = "x";
+position[4][6] = "57162e4ca9ced45b6cf2cdfa";
+position[5][1] = "57162e4ca9ced45b6cf2cdff";
+position[5][2] = "x";
+position[5][3] = "57162e4ca9ced45b6cf2cdfe";
+position[5][4] = "57162e4ca9ced45b6cf2cdfb";
+position[5][5] = "57162e4ca9ced45b6cf2cdfc";
+position[5][6] = "57162e4ca9ced45b6cf2cdfd";
+//*********** Data array end *****************//
+
 server.use(restify.bodyParser());
 
 server.get('/products/', function (req , res, next){
@@ -222,8 +262,66 @@ server.listen(port, function (){
   console.log('%s listening at %s', server.name, port)
 });
 
-
 function screenSend(id, alternativProduct, why){
   console.log(why);
     io.emit(alternativProduct._id, why);
+}
+
+function sendPath(x1, y1, x2, y2){
+  if (x1 < x2) {
+    for (var i = x1; i < position.length; i++) {
+      if(i == x2){
+
+        if (y1 < y2) {
+          for (var k = y1; k < position[i].length; k++) {
+            if(k == y2){
+              if(position[i][k] != 'x'){console.log('done ' + position[i][k]);}
+              break;
+            }
+            if(position[i][k] != 'x'){console.log(position[i][k]);}
+          }
+          break;
+        }else{
+          for (var k = y1; k > 0; k-- ){
+            if(k == y2){
+              if(position[i][k] != 'x'){console.log('done ' + position[i][k]);}
+              break;
+            }
+            if(position[i][k] != 'x'){console.log(position[i][k]);}
+          }
+          break;
+        }
+
+      }
+      if(position[i][y1] != 'x'){console.log(position[i][y1]);}
+    }
+  }else{
+    for (var i = x1; i > 0; i-- ){
+      if(i == x2){
+
+        if (y1 < y2) {
+          for (var k = y1; k < position[i].length; k++) {
+            if(k == y2){
+              if(position[i][k] != 'x'){console.log('done ' + position[i][k]);}
+              break;
+            }
+            if(position[i][k] != 'x'){console.log(position[i][k]);}
+          }
+          break;
+        }else{
+          for (var k = y1; k > 0; k-- ){
+            if(k == y2){
+              if(position[i][k] != 'x'){console.log('done ' + position[i][k]);}
+              break;
+            }
+            if(position[i][k] != 'x'){console.log(position[i][k]);}
+          }
+          break;
+        }
+
+
+      }
+      if(position[i][y1] != 'x'){console.log(position[i][y1]);}
+    }
+  }
 }
